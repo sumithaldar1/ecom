3582d826aa1dbc5cb95a0840ef4c180ead90f152
Sumit Haldar <knuckles@Sumits-MacBook-Pro.local>
2022-05-24 01:40:12 +0530

    Share code

---
+++ src/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Fit Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:150,
+            brand:'Adidas',
+            rating:5,
+            numReviews:90,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Puma Tight Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:200,
+            brand:'Puma',
+            rating:3.5,
+            numReviews:167,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Dior Shirt',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:500,
+            brand:'Dior',
+            rating:4.7,
+            numReviews:40,
+            description:'high quality product'
+        },
+
+    ],
+};
+
+export default data;
