87aeb47134c9cb523e41b6c9233c2b9734279e8c
Sumit Haldar <knuckles@Sumits-MacBook-Pro.local>
2022-05-25 23:16:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import data from './data';
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -13,34 +16,15 @@ function App() {
           <a href="/signin">Sign In</a>
         </div>
       </header>
-      <main>
-        <div className="row center">
-          {
-            data.products.map(product =>(
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                 <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                </div>
-            </div>
-            ))
-          }
-            
-        </div>
-      </main>
+      <main> 
+        <Routes> 
+        <Route path="/product/:id" element={<ProductScreen/>}></Route> 
+        <Route path="/" element={<HomeScreen/>} exact></Route> 
+        </Routes> 
+        </main>
       <footer className="row center">All right reserved</footer>
     </div>
+    </BrowserRouter>
   );
 }
 
